ab6c4e7ffd436ee009075b3afcfffaaf9b0f43a8
GeovanaCastro <geovanacastro2003@gmail.com>
2023-10-15 17:16:14 -0300

    Lista4,Lista5

--- Lista5/1.c
+++ Lista5/1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int i;
-    float S = 0;
-
-    for (i = 50; i >= 1; i--) {
-        S += (float)i / (i + 1);
-    }
-    printf("O valor de S é: %f\n", S);
-    
-    return 0;
+  int i;
+  float S = 0;
+  
+  for (i = 50; i >= 1; i--) {
+    S += (float)i / (i + 1);
+  }
+  
+  printf("O valor de S é: %f\n", S);
+  
+  return 0;
 }
